335e4c91f03afddc1f69624acc906a02f1b6d3be
muammeraltunkan <104419268+Devsetup2@users.noreply.github.com>
2024-12-10 19:05:11 +0300

    script.js

--- script.js
+++ script.js
@@ -454,3 +454,127 @@ const muteVolume = function () {
 }
 
 playerVolumeBtn.addEventListener("click", muteVolume);
+
+
+jQuery(function ($) {
+		$.fn.countTo = function (options) {
+			options = options || {};
+
+			return $(this).each(function () {
+				// set options for current element
+				var settings = $.extend({}, $.fn.countTo.defaults, {
+					from: $(this).data('from'),
+					to: $(this).data('to'),
+					speed: $(this).data('speed'),
+					refreshInterval: $(this).data('refresh-interval'),
+					decimals: $(this).data('decimals')
+				}, options);
+
+				// how many times to update the value, and how much to increment the value on each update
+				var loops = Math.ceil(settings.speed / settings.refreshInterval),
+					increment = (settings.to - settings.from) / loops;
+
+				// references & variables that will change with each update
+				var self = this,
+					$self = $(this),
+					loopCount = 0,
+					value = settings.from,
+					data = $self.data('countTo') || {};
+
+				$self.data('countTo', data);
+
+				// if an existing interval can be found, clear it first
+				if (data.interval) {
+					clearInterval(data.interval);
+				}
+				data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+				// initialize the element with the starting value
+				render(value);
+
+				function updateTimer() {
+					value += increment;
+					loopCount++;
+
+					render(value);
+
+					if (typeof (settings.onUpdate) == 'function') {
+						settings.onUpdate.call(self, value);
+					}
+
+					if (loopCount >= loops) {
+						// remove the interval
+						$self.removeData('countTo');
+						clearInterval(data.interval);
+						value = settings.to;
+
+						if (typeof (settings.onComplete) == 'function') {
+							settings.onComplete.call(self, value);
+						}
+					}
+				}
+
+				function render(value) {
+					var formattedValue = settings.formatter.call(self, value, settings);
+					$self.text(formattedValue);
+				}
+			});
+		};
+
+		$.fn.countTo.defaults = {
+			from: 0, // the number the element should start at
+			to: 0, // the number the element should end at
+			speed: 1000, // how long it should take to count between the target numbers
+			refreshInterval: 100, // how often the element should be updated
+			decimals: 0, // the number of decimal places to show
+			formatter: formatter, // handler for formatting the value before rendering
+			onUpdate: null, // callback method for every time the element is updated
+			onComplete: null // callback method for when the element finishes updating
+		};
+
+		function formatter(value, settings) {
+			return value.toFixed(settings.decimals);
+		}
+
+    //============================================    
+    // y = mx + c where c = y1 given that x=0 is timeStart
+    
+		var now = moment.utc(); //'20141230','YYYYMMDD');
+     var x1 = moment.utc('20140101 00:00.000','YYYYMMDD HH:mm.SSS');
+     var x2 = moment.utc('20150101 00:00.000','YYYYMMDD HH:mm.SSS');
+     var y1 = 2710380995;
+     var y2 = 2923224095;
+     var m = (y2 - y1) / (x2.valueOf() - x1.valueOf());
+     var xNow = now.valueOf()-x1.valueOf(); 
+     var yNow = m * xNow.valueOf() + y1;     
+     var xNowToEnd = x2.valueOf() - now.valueOf();
+    
+     $('#subscribers').attr('data-from',yNow);
+     $('#subscribers').attr('data-speed',xNowToEnd.valueOf());
+     //$('#test').text(yNow);
+
+     $('#subscribers').data('countToOptions', {
+        formatter: function (value, options) {
+          return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, "'");
+        }
+      });
+    
+		// start all the timers
+		$('.timer').each(count);
+
+		function count(options) {
+			var $this = $(this);
+			options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+			$this.countTo(options);
+		}
+    
+    //continue forever at same rate
+    $('#subscribers').data('countToOptions', {
+        onComplete: function (value) {
+          count.call(this, {
+            from: value,
+            to: value + 1
+          });
+        }
+      });
+	});
