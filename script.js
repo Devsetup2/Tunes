60608983772b6c7df86a2aa9254851a2d81d5c6d
muammeraltunkan <104419268+Devsetup2@users.noreply.github.com>
2025-01-22 17:50:26 +0300

    script.js

--- script.js
+++ script.js
@@ -1,496 +1,171 @@
-'use strict';
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("controlIcon");
+const playPauseButton = document.querySelector(".play-pause-btn");
+const forwardButton = document.querySelector(".controls button.forward");
+const backwardButton = document.querySelector(".controls button.backward");
+const songName = document.querySelector(".music-player h1");
+const artistName = document.querySelector(".music-player p");
+
+const songs = [
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
+
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
+  {
+    title: "",
+    name: "",
+    source:
+      "",
+  },
 
 
-
-/**
- * all music information
- */
-
-const musicData = [
-  {
-    backgroundImage: "burnt.webp",
-    posterUrl: "burnt.webp",
-    title: "I'm Burnt",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "./burnt.mp3",
-  },
-  {
-    backgroundImage: "Lazy Tired.webp",
-    posterUrl: "Lazy Tired.webp",
-    title: "Lazy Tired",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "Lazy Tired.mp3",
-  },
-  {
-    backgroundImage: "Deceived.webp",
-    posterUrl: "Deceived.webp",
-    title: "Deceived",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "Deceived.mp3",
-  },
-  {
-    backgroundImage: "Dance Alone.webp",
-    posterUrl: "Dance Alone.webp",
-    title: "Dance Alone",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "Dance Alone .mp3",
-  },
-  {
-    backgroundImage: "Whispers of Autumn.webp",
-    posterUrl: "Whispers of Autumn.webp",
-    title: "Whispers of Autumn",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "Whispers of Autumn.mp3",
-  },
-  {
-    backgroundImage: "summertime.webp",
-    posterUrl: "summertime.webp",
-    title: "Summer Time",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "Summer Time.mp3",
-  },
-{
-    backgroundImage: "My-Years-Without-You.webp",
-    posterUrl: "My-Years-Without-You.webp",
-    title: "My Years Without You",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "My Years Without You.mp3",
-  },
-  {
-    backgroundImage: "fire.webp",
-    posterUrl: "fire.webp",
-    title: "Fire",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "Fire.mp3",
-  },
-  {
-    backgroundImage: "WhatKindLife.webp",
-    posterUrl: "WhatKindLife.webp",
-    title: "What Kind Of Life",
-    album: "Unknown",
-    year: 2025,
-    artist: "Muammer & Suno",
-    musicPath: "WhatKindLife.mp3",
-  },
-  {
-    backgroundImage: "tunes bg.png",
-    posterUrl: "tunes bg.png",
-    title: "?",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "",
-  },
-  {
-    backgroundImage: "tunes bg.png",
-    posterUrl: "tunes bg.png",
-    title: "?",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "",
-  },
-  {
-    backgroundImage: "tunes bg.png",
-    posterUrl: "tunes bg.png",
-    title: "?",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "",
-  },
-  {
-    backgroundImage: "tunes bg.png",
-    posterUrl: "tunes bg.png",
-    title: "?",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "",
-  },
-  {
-    backgroundImage: "tunes bg.png",
-    posterUrl: "tunes bg.png",
-    title: "?",
-    album: "Unknown",
-    year: 2024,
-    artist: "Muammer & Suno",
-    musicPath: "",
-  },
-  
 ];
 
+let currentSongIndex = 3;
 
+function updateSongInfo() {
+  songName.textContent = songs[currentSongIndex].title;
+  artistName.textContent = songs[currentSongIndex].name;
+  song.src = songs[currentSongIndex].source;
 
+  song.addEventListener("loadeddata", function () {});
+}
 
-
-/**
- * add eventListnere on all elements that are passed
- */
-
-const addEventOnElements = function (elements, eventType, callback) {
-  for (let i = 0, len = elements.length; i < len; i++) {
-    elements[i].addEventListener(eventType, callback);
+song.addEventListener("timeupdate", function () {
+  if (!song.paused) {
+    progress.value = song.currentTime;
   }
-}
-
-
-
-/**
- * PLAYLIST
- * 
- * add all music in playlist, from 'musicData'
- */
-
-const playlist = document.querySelector("[data-music-list]");
-
-for (let i = 0, len = musicData.length; i < len; i++) {
-  playlist.innerHTML += `
-  <li>
-    <button class="music-item ${i === 0 ? "playing" : ""}" data-playlist-toggler data-playlist-item="${i}">
-      <img src="${musicData[i].posterUrl}" width="800" height="800" alt="${musicData[i].title} Album Poster"
-        class="img-cover">
-
-      <div class="item-icon">
-        <span class="material-symbols-rounded">equalizer</span>
-      </div>
-    </button>
-  </li>
-  `;
-}
-
-
-
-/**
- * PLAYLIST MODAL SIDEBAR TOGGLE
- * 
- * show 'playlist' modal sidebar when click on playlist button in top app bar
- * and hide when click on overlay or any playlist-item
- */
-
-const playlistSideModal = document.querySelector("[data-playlist]");
-const playlistTogglers = document.querySelectorAll("[data-playlist-toggler]");
-const overlay = document.querySelector("[data-overlay]");
-
-const togglePlaylist = function () {
-  playlistSideModal.classList.toggle("active");
-  overlay.classList.toggle("active");
-  document.body.classList.toggle("modalActive");
-}
-
-addEventOnElements(playlistTogglers, "click", togglePlaylist);
-
-
-
-/**
- * PLAYLIST ITEM
- * 
- * remove active state from last time played music
- * and add active state in clicked music
- */
-
-const playlistItems = document.querySelectorAll("[data-playlist-item]");
-
-let currentMusic = 0;
-let lastPlayedMusic = 0;
-
-const changePlaylistItem = function () {
-  playlistItems[lastPlayedMusic].classList.remove("playing");
-  playlistItems[currentMusic].classList.add("playing");
-}
-
-addEventOnElements(playlistItems, "click", function () {
-  lastPlayedMusic = currentMusic;
-  currentMusic = Number(this.dataset.playlistItem);
-  changePlaylistItem();
 });
 
+song.addEventListener("loadedmetadata", function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+});
 
-
-/**
- * PLAYER
- * 
- * change all visual information on player, based on current music
- */
-
-const playerBanner = document.querySelector("[data-player-banner]");
-const playerTitle = document.querySelector("[data-title]");
-const playerAlbum = document.querySelector("[data-album]");
-const playerYear = document.querySelector("[data-year]");
-const playerArtist = document.querySelector("[data-artist]");
-
-const audioSource = new Audio(musicData[currentMusic].musicPath);
-
-const changePlayerInfo = function () {
-  playerBanner.src = musicData[currentMusic].posterUrl;
-  playerBanner.setAttribute("alt", `${musicData[currentMusic].title} Album Poster`);
-  document.body.style.backgroundImage = `url(${musicData[currentMusic].backgroundImage})`;
-  playerTitle.textContent = musicData[currentMusic].title;
-  playerAlbum.textContent = musicData[currentMusic].album;
-  playerYear.textContent = musicData[currentMusic].year;
-  playerArtist.textContent = musicData[currentMusic].artist;
-
-  audioSource.src = musicData[currentMusic].musicPath;
-
-  audioSource.addEventListener("loadeddata", updateDuration);
-  playMusic();
+function pauseSong() {
+  song.pause();
+  controlIcon.classList.remove("fa-pause");
+  controlIcon.classList.add("fa-play");
 }
 
-addEventOnElements(playlistItems, "click", changePlayerInfo);
-
-/** update player duration */
-const playerDuration = document.querySelector("[data-duration]");
-const playerSeekRange = document.querySelector("[data-seek]");
-
-/** pass seconds and get timcode formate */
-const getTimecode = function (duration) {
-  const minutes = Math.floor(duration / 60);
-  const seconds = Math.ceil(duration - (minutes * 60));
-  const timecode = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-  return timecode;
+function playSong() {
+  song.play();
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
 }
 
-const updateDuration = function () {
-  playerSeekRange.max = Math.ceil(audioSource.duration);
-  playerDuration.textContent = getTimecode(Number(playerSeekRange.max));
-}
-
-audioSource.addEventListener("loadeddata", updateDuration);
-
-
-
-/**
- * PLAY MUSIC
- * 
- * play and pause music when click on play button
- */
-
-const playBtn = document.querySelector("[data-play-btn]");
-
-let playInterval;
-
-const playMusic = function () {
-  if (audioSource.paused) {
-    audioSource.play();
-    playBtn.classList.add("active");
-    playInterval = setInterval(updateRunningTime, 500);
+function playPause() {
+  if (song.paused) {
+    playSong();
   } else {
-    audioSource.pause();
-    playBtn.classList.remove("active");
-    clearInterval(playInterval);
+    pauseSong();
   }
 }
 
-playBtn.addEventListener("click", playMusic);
-
-
-/** update running time while playing music */
-
-const playerRunningTime = document.querySelector("[data-running-time");
-
-const updateRunningTime = function () {
-  playerSeekRange.value = audioSource.currentTime;
-  playerRunningTime.textContent = getTimecode(audioSource.currentTime);
-
-  updateRangeFill();
-  isMusicEnd();
-}
-
-
-
-/**
- * RANGE FILL WIDTH
- * 
- * change 'rangeFill' width, while changing range value
- */
-
-const ranges = document.querySelectorAll("[data-range]");
-const rangeFill = document.querySelector("[data-range-fill]");
-
-const updateRangeFill = function () {
-  let element = this || ranges[0];
-
-  const rangeValue = (element.value / element.max) * 100;
-  element.nextElementSibling.style.width = `${rangeValue}%`;
-}
-
-addEventOnElements(ranges, "input", updateRangeFill);
-
-
-
-/**
- * SEEK MUSIC
- * 
- * seek music while changing player seek range
- */
-
-const seek = function () {
-  audioSource.currentTime = playerSeekRange.value;
-  playerRunningTime.textContent = getTimecode(playerSeekRange.value);
-}
-
-playerSeekRange.addEventListener("input", seek);
-
-
-
-/**
- * END MUSIC
- */
-
-const isMusicEnd = function () {
-  if (audioSource.ended) {
-    playBtn.classList.remove("active");
-    audioSource.currentTime = 0;
-    playerSeekRange.value = audioSource.currentTime;
-    playerRunningTime.textContent = getTimecode(audioSource.currentTime);
-    updateRangeFill();
-  }
-}
-
-
-
-/**
- * SKIP TO NEXT MUSIC
- */
-
-const playerSkipNextBtn = document.querySelector("[data-skip-next]");
-
-const skipNext = function () {
-  lastPlayedMusic = currentMusic;
-
-  if (isShuffled) {
-    shuffleMusic();
-  } else {
-    currentMusic >= musicData.length - 1 ? currentMusic = 0 : currentMusic++;
-  }
-
-  changePlayerInfo();
-  changePlaylistItem();
-}
-
-playerSkipNextBtn.addEventListener("click", skipNext);
-
-
-
-/**
- * SKIP TO PREVIOUS MUSIC
- */
-
-const playerSkipPrevBtn = document.querySelector("[data-skip-prev]");
-
-const skipPrev = function () {
-  lastPlayedMusic = currentMusic;
-
-  if (isShuffled) {
-    shuffleMusic();
-  } else {
-    currentMusic <= 0 ? currentMusic = musicData.length - 1 : currentMusic--;
-  }
-
-  changePlayerInfo();
-  changePlaylistItem();
-}
-
-playerSkipPrevBtn.addEventListener("click", skipPrev);
-
-
-
-/**
- * SHUFFLE MUSIC
- */
-
-/** get random number for shuffle */
-const getRandomMusic = () => Math.floor(Math.random() * musicData.length);
-
-const shuffleMusic = () => currentMusic = getRandomMusic();
-
-const playerShuffleBtn = document.querySelector("[data-shuffle]");
-let isShuffled = false;
-
-const shuffle = function () {
-  playerShuffleBtn.classList.toggle("active");
-
-  isShuffled = isShuffled ? false : true;
-}
-
-playerShuffleBtn.addEventListener("click", shuffle);
-
-
-
-/**
- * REPEAT MUSIC
- */
-
-const playerRepeatBtn = document.querySelector("[data-repeat]");
-
-const repeat = function () {
-  if (!audioSource.loop) {
-    audioSource.loop = true;
-    this.classList.add("active");
-  } else {
-    audioSource.loop = false;
-    this.classList.remove("active");
-  }
-}
-
-playerRepeatBtn.addEventListener("click", repeat);
-
-
-
-/**
- * MUSIC VOLUME
- * 
- * increase or decrease music volume when change the volume range
- */
-
-const playerVolumeRange = document.querySelector("[data-volume]");
-const playerVolumeBtn = document.querySelector("[data-volume-btn]");
-
-const changeVolume = function () {
-  audioSource.volume = playerVolumeRange.value;
-  audioSource.muted = false;
-
-  if (audioSource.volume <= 0.1) {
-    playerVolumeBtn.children[0].textContent = "volume_mute";
-  } else if (audioSource.volume <= 0.5) {
-    playerVolumeBtn.children[0].textContent = "volume_down";
-  } else {
-    playerVolumeBtn.children[0].textContent = "volume_up";
-  }
-}
-
-playerVolumeRange.addEventListener("input", changeVolume);
-
-
-/**
- * MUTE MUSIC
- */
-
-const muteVolume = function () {
-  if (!audioSource.muted) {
-    audioSource.muted = true;
-    playerVolumeBtn.children[0].textContent = "volume_off";
-  } else {
-    changeVolume();
-  }
-}
-
-playerVolumeBtn.addEventListener("click", muteVolume);
-
-
+playPauseButton.addEventListener("click", playPause);
+
+progress.addEventListener("input", function () {
+  song.currentTime = progress.value;
+});
+
+progress.addEventListener("change", function () {
+  playSong();
+});
+
+forwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+backwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+updateSongInfo();
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  centeredSlides: true,
+  initialSlide: 3,
+  slidesPerView: "auto",
+  allowTouchMove: false,
+  spaceBetween: 40,
+  coverflowEffect: {
+    rotate: 25,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".forward",
+    prevEl: ".backward",
+  },
+});
